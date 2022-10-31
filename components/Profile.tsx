5dd5180938031ac7ce52b4280892b68a9788dfd2
Loui Andrae Palo <louiandraepalo@gmail.com>
2022-10-31 12:59:51 +0800

    :sparkles: Auth0 + Nextjs

---
+++ components/Profile.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useUser } from "@auth0/nextjs-auth0";
+
+const Profile = () => {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  return (
+    user && (
+      <div>
+        <img src={user.picture} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <p>{user.sub}</p>
+      </div>
+    )
+  );
+};
+
+export default Profile;
